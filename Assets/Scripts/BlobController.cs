68a4dd87fd429440f482b0ebba1b0bcfc7c2562d
Kane Clover <kane@assure24.com>
2016-07-22 17:18:39 +0100

    Precompute distances and only affect blob on mouse down

--- Assets/Scripts/BlobController.cs
+++ Assets/Scripts/BlobController.cs
@@ -6,22 +6,29 @@ public class BlobController : MonoBehaviour {
 //	private Rigidbody rb;
 	public float sensitivity;
 	public float speed;
+	public GameObject floor;
+	private Camera camera;
+	private float cam_distance;
 
 	void Start () {
 //		rb = GetComponent<Rigidbody> ();
+		camera = Camera.main.GetComponent<Camera>();
+		cam_distance = Vector3.Distance (camera.transform.position, floor.transform.position);
 	}
 	
 	void FixedUpdate () {
 
+		// Cursor keys and force
 //		float haxis = Input.GetAxis ("Horizontal");
 //		float vaxis = Input.GetAxis ("Vertical");
 //		Vector3 force = new Vector3 (haxis, 0.0f, vaxis);
 //		rb.AddForce (force*sensitivity);
 
-		Camera camera = Camera.main.GetComponent<Camera>();
-		// 20f = distance of camera from floor
-		Vector3 p = camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 20f ));
-		Debug.logger.Log (p);
-		transform.position = p;
+		// Pinned to Mouse (also works with touch on mobile)
+		if (Input.GetMouseButton(0)) {
+			Vector3 p = camera.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, cam_distance));
+			Debug.logger.Log (cam_distance);
+			transform.position = p;
+		} 
 	}
 }
