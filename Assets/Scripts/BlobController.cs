4690f5bed386ce7dd605c7fd597848d8703c88c1
Kane Clover <kane@assure24.com>
2016-07-22 12:44:29 +0100

    Direct mouse control

--- Assets/Scripts/BlobController.cs
+++ Assets/Scripts/BlobController.cs
@@ -3,24 +3,25 @@ using System.Collections;
 
 public class BlobController : MonoBehaviour {
 
-	private Rigidbody rb;
+//	private Rigidbody rb;
 	public float sensitivity;
-//	public GameObject slave;
+	public float speed;
 
-	// Use this for initialization
 	void Start () {
-		rb = GetComponent<Rigidbody> ();
+//		rb = GetComponent<Rigidbody> ();
 	}
 	
-	// Update is called once per frame
 	void FixedUpdate () {
-		float haxis = Input.GetAxis ("Horizontal");
-		float vaxis = Input.GetAxis ("Vertical");
-		Vector3 force = new Vector3 (haxis, 0.0f, vaxis);
-		rb.AddForce (force*sensitivity);
-	}
-	void Update() {
-//		slave.transform.position = this.transform.position;
-//		slave.transform.RotateAround (Vector3.zero, Vector3.up, 90.0f);
+
+//		float haxis = Input.GetAxis ("Horizontal");
+//		float vaxis = Input.GetAxis ("Vertical");
+//		Vector3 force = new Vector3 (haxis, 0.0f, vaxis);
+//		rb.AddForce (force*sensitivity);
+
+		Camera camera = Camera.main.GetComponent<Camera>();
+		// 20f = distance of camera from floor
+		Vector3 p = camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 20f ));
+		Debug.logger.Log (p);
+		transform.position = p;
 	}
 }
