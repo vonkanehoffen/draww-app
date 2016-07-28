2102d7e21f80fdaceccfd1e7c4c728729f2031b1
Kane Clover <kane@assure24.com>
2016-07-28 14:49:55 +0100

    Freeze current drawing as texture on floor

---
+++ Assets/Scripts/FloorController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloorController : MonoBehaviour {
+
+	private Renderer rend;
+
+	void Start () {
+		rend = GetComponent<Renderer> ();
+	}
+
+	public void FreezeImage() {
+		// TRhis is necessary so the capture happens after drawing.
+		StartCoroutine (SetTexture ());
+	}
+
+	IEnumerator SetTexture() {
+		yield return new WaitForEndOfFrame();
+
+		int w = Screen.width;
+		int h = Screen.height;
+
+		Texture2D tex = new Texture2D(w, w);
+		tex.ReadPixels(new Rect(0, h-w, w, w), 0, 0);
+		tex.Apply();
+		rend.material.mainTexture = tex;
+	}
+
+}
