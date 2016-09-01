851de882440f0fd3a186dff5f78b821e98d6755f
Kane Clover <kane@assure24.com>
2016-09-01 23:23:09 +0100

    Upgrade to Unity 5.4.0 + working webcam square

---
+++ Assets/Scripts/CamAttach.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamAttach : MonoBehaviour {
+
+	// NatCam? https://www.assetstore.unity3d.com/en/#!/content/52154
+	// http://forum.unity3d.com/threads/webcamtexture-issue.392993/
+
+	void Start () {
+		WebCamTexture webcamTexture = new WebCamTexture();
+		Renderer renderer = GetComponent<Renderer>();
+		renderer.material.mainTexture = webcamTexture;
+		webcamTexture.Play();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
