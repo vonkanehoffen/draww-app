73654fdb28ad5456cae100367fad830f7f0d5013
Kane Clover <kane@assure24.com>
2016-09-02 12:07:24 +0100

    Texture scale temporary control (running fine on Android)

--- Assets/Scripts/CamAttach.cs
+++ Assets/Scripts/CamAttach.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class CamAttach : MonoBehaviour {
 
 	public GameObject[] faces;
+	public float textureScale;
 
 	// NatCam? https://www.assetstore.unity3d.com/en/#!/content/52154
 	// http://forum.unity3d.com/threads/webcamtexture-issue.392993/
@@ -13,8 +14,16 @@ public class CamAttach : MonoBehaviour {
 		foreach (GameObject face in faces) {
 			Renderer renderer = face.GetComponent<Renderer> ();
 			renderer.material.mainTexture = webcamTexture;
+			renderer.material.mainTextureScale = new Vector2 (textureScale, textureScale);
 		}
 		webcamTexture.Play();
+	}
+
+	void Update () {
+//		foreach (GameObject face in faces) {
+//			Renderer renderer = face.GetComponent<Renderer> ();
+//			renderer.material.mainTextureScale = new Vector2 (textureScale, textureScale);
+//		}
 	}
 
 }
