b91803b27db8d4f4a0d5cf57663b001565c18548
Kane Clover <kane@assure24.com>
2016-09-02 09:18:49 +0100

    Twooo WebCamTextures, ah ah ah

--- Assets/Scripts/CamAttach.cs
+++ Assets/Scripts/CamAttach.cs
@@ -3,13 +3,18 @@ using System.Collections;
 
 public class CamAttach : MonoBehaviour {
 
+	public GameObject target1;
+	public GameObject target2;
+
 	// NatCam? https://www.assetstore.unity3d.com/en/#!/content/52154
 	// http://forum.unity3d.com/threads/webcamtexture-issue.392993/
 
 	void Start () {
 		WebCamTexture webcamTexture = new WebCamTexture();
-		Renderer renderer = GetComponent<Renderer>();
-		renderer.material.mainTexture = webcamTexture;
+		Renderer renderer1 = target1.GetComponent<Renderer>();
+		Renderer renderer2 = target2.GetComponent<Renderer>();
+		renderer1.material.mainTexture = webcamTexture;
+		renderer2.material.mainTexture = webcamTexture;
 		webcamTexture.Play();
 	}
 
