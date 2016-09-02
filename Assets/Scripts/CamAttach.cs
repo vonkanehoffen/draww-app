60a23050e6ef6ae73715ac8ff759bd5fc195b574
Kane Clover <kane@assure24.com>
2016-09-02 10:11:09 +0100

    Apply WebCamTexture to array of objects

--- Assets/Scripts/CamAttach.cs
+++ Assets/Scripts/CamAttach.cs
@@ -3,23 +3,18 @@ using System.Collections;
 
 public class CamAttach : MonoBehaviour {
 
-	public GameObject target1;
-	public GameObject target2;
+	public GameObject[] faces;
 
 	// NatCam? https://www.assetstore.unity3d.com/en/#!/content/52154
 	// http://forum.unity3d.com/threads/webcamtexture-issue.392993/
 
 	void Start () {
 		WebCamTexture webcamTexture = new WebCamTexture();
-		Renderer renderer1 = target1.GetComponent<Renderer>();
-		Renderer renderer2 = target2.GetComponent<Renderer>();
-		renderer1.material.mainTexture = webcamTexture;
-		renderer2.material.mainTexture = webcamTexture;
+		foreach (GameObject face in faces) {
+			Renderer renderer = face.GetComponent<Renderer> ();
+			renderer.material.mainTexture = webcamTexture;
+		}
 		webcamTexture.Play();
 	}
 
-	// Update is called once per frame
-	void Update () {
-
-	}
 }
